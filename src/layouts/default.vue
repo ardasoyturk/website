c0f4865b38357e9813d7ce427833b17595fc3c76
Arda Soytürk <33841775+ardasoyturk@users.noreply.github.com>
2021-04-16 17:44:06 +0300

    Added new scrollbars

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,3 +3,25 @@
     <Nuxt />
   </div>
 </template>
+
+<style>
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+</style>
