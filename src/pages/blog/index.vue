17211065c3d6c3a239f516e0805a88e648795918
Arda Soytürk <33841775+ardasoyturk@users.noreply.github.com>
2021-04-16 17:49:06 +0300

    WIP page for Blog

---
+++ src/pages/blog/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="grid w-full min-h-screen bg-gray-50 place-items-center place-content-center">
+    <h1 class="text-xl uppercase font-quicksand">work in progress...</h1>
+  </div>
+</template>
