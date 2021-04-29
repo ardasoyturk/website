b7f00e2e5f862729b22485d1550f708824eb4130
Arda Soytürk <33841775+ardasoyturk@users.noreply.github.com>
2021-04-29 18:04:45 +0300

    refactor: made the icons bigger

--- src/components/Icons.vue
+++ src/components/Icons.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex justify-center space-x-1.5 text-xl text-center select-none">
-		<a content="Arda#0061" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.discord"><unicon name="discord" fill="#7289DA" width="24" height="24" icon-style="monochrome"></unicon></a>
-		<a content="soyturkarda_" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.twitter"><unicon name="twitter" fill="#06B6D4" width="24" height="24" icon-style="line"></unicon></a>
-		<a content="ardasoyturk" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.github"><unicon name="github" fill="#111827" width="24" height="24" icon-style="line"></unicon></a>
-		<a content="ardasoyturk" class="self-center" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.spotify"><unicon name="spotify" width="21" height="21" icon-style="line"></unicon></a>
+		<a content="Arda#0061" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.discord"><unicon name="discord" fill="#7289DA" width="36" height="36" icon-style="monochrome"></unicon></a>
+		<a content="soyturkarda_" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.twitter"><unicon name="twitter" fill="#06B6D4" width="36" height="36" icon-style="line"></unicon></a>
+		<a content="ardasoyturk" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.github"><unicon name="github" fill="#111827" width="36" height="36" icon-style="line"></unicon></a>
+		<a content="ardasoyturk" class="self-center" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.spotify"><unicon name="spotify" width="33" height="33" icon-style="line"></unicon></a>
     <!-- <a content="E-Mail" v-tippy="{ placement : 'bottom',  arrow: true }" :href="getURL('mail')"><unicon name="envelope" fill="#DC2626" width="32" height="32" icon-style="line"></unicon></a> -->
    </div>
 </template>
