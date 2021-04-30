ffdbac08f8a83f914a1734939f4e5997b1938b02
Arda Soytürk <33841775+ardasoyturk@users.noreply.github.com>
2021-04-30 20:55:39 +0300

    refactor: moved icon links to index

--- src/components/Icons.vue
+++ src/components/Icons.vue
@@ -3,24 +3,13 @@
 		<a content="Arda#0061" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.discord"><unicon name="discord" fill="#7289DA" width="36" height="36" icon-style="monochrome"></unicon></a>
 		<a content="soyturkarda_" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.twitter"><unicon name="twitter" fill="#06B6D4" width="36" height="36" icon-style="line"></unicon></a>
 		<a content="ardasoyturk" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.github"><unicon name="github" fill="#111827" width="36" height="36" icon-style="line"></unicon></a>
-		<a content="ardasoyturk" class="self-center" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.spotify"><unicon name="spotify" width="33" height="33" icon-style="line"></unicon></a>
+		<a content="ardasoyturk" class="self-center" v-tippy="{ placement : 'bottom',  arrow: true }" :href="this.links.spotify"><unicon fill="#00D95F" name="spotify" width="33" height="33" icon-style="line"></unicon></a>
     <!-- <a content="E-Mail" v-tippy="{ placement : 'bottom',  arrow: true }" :href="getURL('mail')"><unicon name="envelope" fill="#DC2626" width="32" height="32" icon-style="line"></unicon></a> -->
    </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      links: {
-        discord: 'https://discord.com/users/403225819222245377',
-        twitter: 'https://twitter.com/soyturkarda_',
-        github: 'https://github.com/ardasoyturk',
-        spotify: 'https://open.spotify.com/user/zsfhpjxcu5uvcwjnlt5k2vvt9',
-        telegram: 'https://t.me/ardasoyturk',
-        /* mail: 'mailto:' */
-      },
-    };
-  },
+  props: ['links']
 };
 </script>
