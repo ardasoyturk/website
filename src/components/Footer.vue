be7e559cd3b7dba1da46b8cd833fbfc178c6b7c8
Arda Soytürk <33841775+ardasoyturk@users.noreply.github.com>
2021-04-30 20:56:34 +0300

    chore: new design with footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="text-gray-600 body-font">
+  <div class="bg-gray-200">
+    <div class="container flex flex-col flex-wrap px-5 py-3 mx-auto sm:flex-row">
+      <p class="self-center text-sm text-center text-gray-500 sm:text-left">© {{ new Date().getFullYear() }} Arda Soyturk —
+        <span class="text-gray-600">This website's source code is on <a href="https://github.com/ardasoyturk/website" rel="noopener noreferrer" class="underline">GitHub</a>.</span> 
+      </p>
+      <span class="inline-flex items-center justify-center mt-2 space-x-0.5 sm:ml-auto sm:mt-0 sm:justify-start">
+      <a :href="this.links.discord"><unicon name="discord" fill="currentColor" width="24" height="24" icon-style="monochrome"></unicon></a>
+		  <a :href="this.links.twitter"><unicon name="twitter" fill="currentColor" width="24" height="24" icon-style="line"></unicon></a>
+		  <a :href="this.links.github"><unicon name="github" fill="currentColor" width="24" height="24" icon-style="line"></unicon></a>
+		  <a :href="this.links.spotify"><unicon name="spotify" fill="currentColor" width="21" height="21" icon-style="line"></unicon></a>
+      </span>
+    </div>
+  </div>
+  <div class="w-full h-3 hover:bg-[#182e50] bg-[#12233c]"></div>
+</footer>
+</template>
+
+<script>
+export default {
+  props: ['links']
+}
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-	<div>
-		<div class="flex items-center justify-center w-full min-h-screen md:h-screen">
+	<div class="bg-gray-50">
+		<div class="relative flex items-center justify-center w-full min-h-screen md:h-screen">
+			<div class="absolute top-0 w-full h-3 hover:bg-[#182e50] bg-[#12233c]"></div>
 			<div
 				class="flex flex-col-reverse justify-between rounded-full sm:flex-row"
 			>
@@ -19,9 +20,6 @@
 					/>
 				</div>
 			</div>
-        <a
-          class="absolute cursor-pointer bottom-8 scroll"
-        ></a>
 		</div>
     <div class="relative flex items-center justify-center w-full min-h-screen py-10">
 				<div class="flex items-center mx-auto justify-centerh-screen lg:grid">
@@ -35,12 +33,12 @@
 							<h5 class="text-3xl font-medium tracking-tight text-center font-poppins">
 								My accounts on social media
 							</h5>
-							<Icons />
+							<Icons :links="links" />
 						</div>
 					</div>
 				</div>
-        <div class="absolute bottom-0 w-full h-3 bg-[#12233c]"></div>
     </div>
+		<Footer :links="links" />
 	</div>
 </template>
 
@@ -54,100 +52,15 @@ export default {
 	components: { AboutMe, Positions, Projects },
   data() {
     return {
-
+			links: {
+        discord: 'https://discord.com/users/403225819222245377',
+        twitter: 'https://twitter.com/soyturkarda_',
+        github: 'https://github.com/ardasoyturk',
+        spotify: 'https://open.spotify.com/user/zsfhpjxcu5uvcwjnlt5k2vvt9',
+        telegram: 'https://t.me/ardasoyturk',
+        /* mail: 'mailto:' */
+      }
     }
   }
 };
 </script>
-
-
-<style>
-:root {
-	--scroll-color: #333;
-}
-.scroll {
-	width: 40px;
-	height: 40px;
-	border: 3px solid var(--scroll-color);
-	border-radius: 50%;
-	animation: down 1.5s infinite;
-	-webkit-animation: down 1.5s infinite;
-	&::before {
-		content: "";
-		position: absolute;
-		top: 8px;
-		left: 12px;
-		width: 12px;
-		height: 12px;
-		border-bottom: 3px solid var(--scroll-color);
-		border-left: 3px solid var(--scroll-color);
-		transform: rotate(-45deg);
-	}
-}
-.gotop {
-	width: 40px;
-	height: 40px;
-	border: 3px solid var(--scroll-color);
-	border-radius: 50%;
-	animation: up 1.5s infinite;
-	-webkit-animation: up 1.5s infinite;
-	&::before {
-		content: "";
-		position: absolute;
-		bottom: 8px;
-		left: 12px;
-		width: 12px;
-		height: 12px;
-		border-right: 3px solid #333;
-		border-top: 3px solid #333;
-		transform: rotate(-45deg);
-	}
-}
-@keyframes down {
-	0% {
-		transform: translate(0);
-	}
-	20% {
-		transform: translateY(15px);
-	}
-	40% {
-		transform: translate(0);
-	}
-}
-@-webkit-keyframes down {
-	0% {
-		transform: translate(0);
-	}
-	20% {
-		transform: translateY(15px);
-	}
-	40% {
-		transform: translate(0);
-	}
-}
-@keyframes up {
-	0% {
-		transform: translate(0);
-	}
-	20% {
-		transform: translateY(-15px);
-	}
-	40% {
-		transform: translate(0);
-	}
-}
-@-webkit-keyframes up {
-	0% {
-		transform: translate(0);
-	}
-	20% {
-		transform: translateY(-15px);
-	}
-	40% {
-		transform: translate(0);
-	}
-}
-.section {
-	@apply w-screen h-screen;
-}
-</style>
