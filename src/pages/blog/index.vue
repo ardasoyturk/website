936ad1e04d456f67831959f80f330bead729dd22
Arda Soytürk <33841775+ardasoyturk@users.noreply.github.com>
2021-05-03 00:23:15 +0300

    refactor: forgot to remove background from blog wip page

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid w-full min-h-screen bg-gray-50 place-items-center place-content-center">
-    <h1 class="text-xl uppercase font-quicksand">work in progress...</h1>
+  <div class="grid w-full min-h-screen place-items-center place-content-center">
+    <h1 class="text-xl uppercase font-quicksand dark:text-gray-50">work in progress...</h1>
   </div>
 </template>
