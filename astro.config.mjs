47e059d1a4b45acf16e626f121777f897781c279
Arda Soyturk <33841775+ardasoyturk@users.noreply.github.com>
2026-03-05 23:25:26 +0300

    feat: integrate Cloudflare adapter and update configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { fileURLToPath } from "node:url";
 
+import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
